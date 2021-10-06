b55913d695824456cc66bd8c04f16f6e58b577e5
Hg Hg <hghghghg.hh39@gmail.com>
2021-10-06 11:52:34 +0300

    feat: parsing from labirint & oz

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,18 +5,45 @@ import io.ktor.application.*
 import io.ktor.response.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.lang.StringBuilder
 
 fun Application.configureRouting() {
+
+    fun Element.getBelarusianPrice(): Double? = text().substringBefore(" ").replace(',','.').toDoubleOrNull()
+
+    fun String.getHTML(): Document = Jsoup.connect(this).get()
+
     val api = "/api"
+
+    val NOT_PRESENTED = 999999999
+    val NOT_PRESENTED_DOUBLE = 999999999.0
 
     routing {
         get(path = "$api/book={bookName}") {
-            val searchPage: Document = Jsoup.connect("https://yandex.by/search/?text=${call.parameters["bookName"]}+вайлдберриз").get()
-            val bookUrl = searchPage.select("a[class=\"Link Link_theme_outer Path-Item link path__item organic__greenurl\"]")[0].attr("href")
-            val bookPage: Document = Jsoup.connect("$bookUrl").get()
-            val title: String = bookPage.select("span[data-link=\"text{:productCard^goodsName}\"]").text()
-            val price: String = bookPage.select("span[class=\"price-block__commission-current-price\"]")[0].text().removeSuffix(" ₽").replace(" ", "")
-            call.respondText("{title: \"$title\", price: $price}")
+            val bookName = call.parameters["bookName"]
+            val oz = "https://oz.by/search/?q=$bookName".getHTML()
+            val priceOz = run {
+                val priceIfSingleBookOnOz =
+                    oz.select("span[class=\"b-product-control__text b-product-control__text_main\"]")
+                if (priceIfSingleBookOnOz.isNotEmpty()) priceIfSingleBookOnOz[0].getBelarusianPrice() else null
+            } ?: oz.select("span[class=\"item-type-card__btn\"]")
+                .map { it.getBelarusianPrice() ?: NOT_PRESENTED_DOUBLE }
+                .minOf { it }
+
+            val labirint = "https://www.labirint.ru/search/$bookName/?stype=0".getHTML()
+            val pricesLabirint = labirint.select("span[class=\"price-val\"]")
+            val priceLabirint =
+                if (pricesLabirint.isNotEmpty()) {
+                    pricesLabirint
+                        .map { it.text().substringBefore(" ₽").replace(" ", "").toIntOrNull() }
+                        .minOf { it ?: NOT_PRESENTED }
+                } else { NOT_PRESENTED }
+
+            val response = StringBuilder("{bookName: \"$bookName\"")
+            if (priceOz != NOT_PRESENTED_DOUBLE) response.append(", priceOz: $priceOz")
+           if (priceLabirint != NOT_PRESENTED) response.append(", priceLabirint: $priceLabirint")
+            call.respondText("$response}")
         }
         get("/") {
             call.respondText("Hello World! To use this API, see documentation in source code.")
